456213ec6ecee23dc359a6ac959ba11c51b8a0a2
Ray <unaviz2022@gmail.com>
2025-06-23 23:53:26 -0500

    Actualización del proyecto fiscalizacion_classification

--- settings/config.py
+++ settings/config.py
@@ -16,54 +16,9 @@ _config = Config(ENV_DIR)
 
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
-# Azure Open AI
-AZURE_OPENAI_API_KEY = _config("AZURE_OPENAI_API_KEY")
-AZURE_OPENAI_ENDPOINT = _config("AZURE_OPENAI_ENDPOINT")
-AZURE_OPENAI_DEPLOYMENT_NAME = _config("AZURE_OPENAI_DEPLOYMENT_NAME")
-AZURE_OPENAI_API_VERSION = _config("AZURE_OPENAI_API_VERSION")
-AZURE_OPENAI_TEMPERATURE = _config("AZURE_OPENAI_TEMPERATURE") # Valor por defecto 0.5
-AZURE_OPENAI_TOP_P = _config("AZURE_OPENAI_TOP_P") # Valor por defecto 100
-AZURE_OPENAI_EMBEDDING_API_VERSION = _config("AZURE_OPENAI_EMBEDDING_API_VERSION")  
-AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME = _config("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME")      
-
-# CosmosDB
-COSMOSDB_ENDPOINT = _config("COSMOSDB_ENDPOINT")
-COSMOSDB_PRIMARY_KEY = _config("COSMOSDB_PRIMARY_KEY")
-COSMOSDB_DATABASE = _config("COSMOSDB_DB_NAME")
-COSMOSDB_CONTAINER = _config("COSMOSDB_CONTAINER")
-
-# Storage Account
-AZURE_STORAGE_CONNECTION_STRING = _config("AZURE_STORAGE_CONNECTION_STRING")
-AZURE_STORAGE_CONTAINER = _config("AZURE_STORAGE_CONTAINER")
-
-def create_app():
-    app = FastAPI(
-        title="Classificator Alcaldia de Bogota",
-        description="API documentation",
-        version="0.0.1",
-        contact={
-            "name": "Pi Data & Consulting",
-            "url": "https://piconsulting.com.ar/contacto/",
-            "email": "info@piconsulting.com.ar",
-        })
-
-    origins = ["*"]
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    return app
-
-
-
-def load_spacy_model(model_name="es_core_news_md"):
-    try:
-        return spacy.load(model_name)
-    except OSError:
-        print(f"Modelo '{model_name}' no encontrado. Descargando...")
-        download(model_name)
-        return spacy.load(model_name)
+#AI Speach project
+AZURE_OPEN_AI_SPEACH=_config("AZURE_OPEN_AI_SPEACH")
+AZURE_OPEN_AI_AUTOMATIZACION=_config("AZURE_OPEN_AI_AUTOMATIZACION")
+AZURE_OPEN_AI_SEARCH=_config("AZURE_OPEN_AI_SEARCH")
+AZURE_OPEN_AI_SEARCH_RESOLUCION=_config("AZURE_OPEN_AI_SEARCH_RESOLUCION")
+AZURE_OPEN_AI_SEARCH_NORMATIVA=_config("AZURE_OPEN_AI_SEARCH_NORMATIVA")
